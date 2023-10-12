f433fb6f7e053670749fe780aadf1ba23967a1a2
Diogo Batista <d.s.batista@icloud.com>
2023-10-12 18:29:04 +0100

    Inicio da estilização /LandingPage feita

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -5,13 +5,13 @@ function LandingPage() {
 
 
   return (
-    <div className="landing-page">
-      <div className="header">
-        <h1 className="app-name">Booked</h1>
+    <div className="landing">
+      <div className="top">
+        <h1>Booked</h1>
       </div>
-      <div className="content">
-        <h1 className="welcome-text">Welcome to <span className="colored-text">Booked</span></h1>
-        <h3 className="description">A place where you can write your book review</h3>
+      <div className="rest">
+        <h1>Welcome to <span className="colored-text">Booked</span></h1>
+        <h3>A place where you can write your book review</h3>
        <Link to ="/" className='btngohome'>Enter Website</Link>
       </div>
     </div>
