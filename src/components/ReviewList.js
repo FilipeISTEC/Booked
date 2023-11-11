bc6fc6cc51d1441c0a56c21c8485195649df4e3a
Diogo Batista <d.s.batista@icloud.com>
2023-11-11 15:52:03 +0000

    ReviewStar bug

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 import "../assets/styles/ReviewList.css";
 import "../assets/utils/getCurrentDate.js";
 import getCurrentDate from "../assets/utils/getCurrentDate";
-import Rating from "./Rating";
+// import Rating from "./Rating";
+import StarRating from "./StarRating.js";
 
-const ReviewList = ({ reviews, title, date , rating }) => {
+const ReviewList = ({ reviews, title, date, rating }) => {
   const displayDate = date === undefined ? getCurrentDate() : date;
 
   return (
@@ -20,7 +21,7 @@ const ReviewList = ({ reviews, title, date , rating }) => {
                 <p>By</p>
 
                 <h3>{review.author}</h3>
-                <p>{review.rating}</p>
+                <StarRating rating={review.rating} />
                 <p className="Hora">{displayDate}</p>
                 <p className="PreviewText">{review.body.substring(0, 400)}</p>
               </div>
--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import "../assets/styles/ratingStar.css";
 import starEmptyImage from "../assets/images/star-empty.svg";
+import starFilledImage from "../assets/images/star-filled.svg";
 
 function StarRating() {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(null);
 
   return (
-    <div className="empty-star">
+    <div className="star-rating">
       {[...Array(5)].map((_, index) => {
         const currentRating = index + 1;
         return (
@@ -19,16 +20,16 @@ function StarRating() {
               onClick={() => setRating(currentRating)}
             />
             <img
-              src={starEmptyImage}
+              src={
+                currentRating <= (hover || rating)
+                  ? starFilledImage
+                  : starEmptyImage
+              }
               alt={`Star ${index + 1}`}
               className="star"
               style={{
                 cursor: "pointer",
-                filter:
-                  currentRating <= (hover || rating)
-                    ? "#F9EE00"
-                    : //#F9EE00
-                      "brightness(10%)",
+                opacity: currentRating <= (hover || rating) ? 1 : 0.5,
               }}
               onMouseEnter={() => setHover(currentRating)}
               onMouseLeave={() => setHover(null)}
